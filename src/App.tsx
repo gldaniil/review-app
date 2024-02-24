f7b6a4b2818efe670139f13e29b439cec594bb54
gldaniil <daniil.gajfullin@yandex.ru>
2024-02-25 03:46:20 +0500

    Составляю начальную разметку

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import styles from "./App.module.scss";
+
 const App = () => {
-  return <h1>Review App</h1>;
+  return (
+    <div className={styles.wrapper}>
+      <header>header</header>
+      <main className={styles.main}>main</main>
+      <footer>footer</footer>
+    </div>
+  );
 };
 
 export default App;
